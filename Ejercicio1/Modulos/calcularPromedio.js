37a60aa9e37f4cca735e3f480e940625ad3cccf9
Luis Villamizar <Luisca123c@gmail.com>
2025-11-24 17:14:20 -0500

    Ejercicio 1 comentado

--- Ejercicio1/Modulos/calcularPromedio.js
+++ Ejercicio1/Modulos/calcularPromedio.js
@@ -1,9 +1,14 @@
+// creamos una funcion declarada para calcular el promedio de un arreglo de notas
 export function calcularPromedio(notas) {
+    // inicializamos la variable total_notas en 0
     let total_notas = 0;
+    // recorremos el arreglo de notas y sumamos cada nota a total_notas
     for (let i = 0; i < notas.length; i++) {
         total_notas += notas[i];
     }
+    // calculamos el promedio dividiendo el total de notas entre la cantidad de notas
     let promedio = total_notas / notas.length;
+    // evaluamos el promedio y retornamos un mensaje según el rango en el que se encuentra
     if (promedio >= 4.5)
     {
         return `El estudiantes tiene promedio alto con ${promedio}`;
--- Ejercicio1/app.js
+++ Ejercicio1/app.js
@@ -1,19 +1,27 @@
+//Importar la función calcularPromedio desde el módulo correspondiente
 import {calcularPromedio} from "./Modulos/calcularPromedio.js";
-
+// Inicializamos variable de cantidad y un arreglo para las notas
 let cant = 0;
 const notas = [];
+// preguntamos al usuario la cantidad de notas
 cant = parseInt(prompt("Ingrese la cantidad de notas del estudiante:"));
+// Recolectamos las notas del estudiante en un ciclo for
 for (let i = 0; i < cant; i++) {
+    // inicializamos la variable nota
     let nota = 0;
+    // Pedimos la nota al usuario
     nota = parseFloat(prompt(`Ingrese la nota ${i + 1}:`));
+    // Validamos que la nota sea mayor a 0 antes de agregarla al arreglo
     if (nota > 0 )
     {
         notas.push(nota);
     }
+    // Si la nota no es válida, mostramos una alerta y repetimos la iteración
     else
     {
         alert("La nota debe ser mayor a 0");
         i--;
     }
 }
+// Llamamos a la función calcularPromedio y mostramos el resultado en una alerta
 alert(calcularPromedio(notas));
